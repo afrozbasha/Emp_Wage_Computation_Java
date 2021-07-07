dfe06aa37feb440b8f0b9e9b66c99bdbf4961189
afrozbasha <iafrozbasha@gmail.com>
2021-07-07 23:58:56 +0530

    This is Program for Employee wage Computation

---
+++ EmpWageComputationUC2.java
@@ -0,0 +1,21 @@
+package com.EmpWageComputationUC2;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        int Wage_HR = 20;
+        int Day_HRs = 8;
+        Random random = new Random();
+        int r = random.nextInt(2);
+        if(r == 1){
+            System.out.println("Employee Present");
+            System.out.println("DayWage = " + Wage_HR * Day_HRs);
+        }
+        else
+            System.out.println("Employee Absent");
+
+    }
+}
